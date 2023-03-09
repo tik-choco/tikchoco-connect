5e903bb03345f5ee41077a7ef23e16a153f2e443
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-09 18:48:19 +0900

    asset update

---
+++ js/page.js
@@ -0,0 +1,45 @@
+var slider;
+var sliderFlag = false;
+var breakpoint = 768;//768px以下の場合
+  
+function sliderSet() {
+        var windowWidth = window.innerWidth;
+        if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
+            slider = $('.slider').bxSlider({
+            touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
+			mode: 'vertical',//縦スライド指定
+			controls: false,//前後のコントロールを表示させない。
+			auto: 'true',//自動的にスライド
+			pager: false//ページ送り無効化
+		});
+            sliderFlag = true;
+        } else if (windowWidth < breakpoint && sliderFlag) {
+            slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
+            sliderFlag = false;
+        }
+    }
+
+$(window).on('load resize', function() {
+        sliderSet();
+});
+
+$('.slider').bxSlider({
+    touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
+    mode: 'vertical',
+    controls: false,
+    auto: 'true',
+    pager: false
+    });
+
+	$('.slider2').slick({
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		speed: 500,//スライドのスピード。初期値は300。
+		slidesToShow: 3,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		centerMode: true,//要素を中央ぞろえにする
+		variableWidth: true,//幅の違う画像の高さを揃えて表示
+		dots: true,//下部ドットナビゲーションの表示
+	});
