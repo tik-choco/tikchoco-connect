5baa4976e6fbef4bc9ead02a66b655c9a594fc47
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-11 22:45:06 +0900

    ajax

--- js/portal.js
+++ js/portal.js
@@ -21,28 +21,42 @@ function footer(){
     });
 }
 
-// function header(rootDir){
-//     $.ajax({
-//         url: rootDir + "header.html",  // 読み込むHTMLファイル
-//         cache: false,
-//         async: false,
-//         dataType: 'html',
-//         success: function(html){
-//             html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
-//             document.write(html);
-//         }
-//     });
-// }
+$(function(){
+	//ページを表示させる箇所の設定
+	var $content = $('.pageDisplay');
+	//ボタンをクリックした時の処理
+	$(document).on('click', '.gnavi a', function(event) {
+		event.preventDefault();
+		//.gnavi aのhrefにあるリンク先を保存
+		var link = $(this).attr("href");
+		//リンク先が今と同じであれば遷移しない
+		if(link == lastpage){
+			return false;
+		}else{
+			$content.fadeOut(600, function() {
+				getPage(link);
+			});
+			//今のリンク先を保存
+			lastpage = link;
+		}
+		
+	});
+	//初期設定
+	getPage("1.html");
+	var lastpage = "1.html";
 
-// function footer(rootDir){
-//     $.ajax({
-//         url: rootDir + "footer.html",  // 読み込むHTMLファイル
-//         cache: false,
-//         async: false,
-//         dataType: 'html',
-//         success: function(html){
-//             html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
-//             document.write(html);
-//         }
-//     });
-// }
+	//ページを取得してくる
+    function getPage(elm){
+    	$.ajax({
+            type: 'GET',
+            url: elm,
+            dataType: 'html',
+            success: function(data){
+                $content.html(data).fadeIn(600);
+            },
+            error:function() {
+                       alert('問題がありました。');
+                   }
+    	});
+    }
+});
