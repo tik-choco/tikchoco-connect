4696f1871bcfec1c93fc12c6b71d2b98610509d5
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-11 22:24:50 +0900

    beta:audate:ajax header

--- js/portal.js
+++ js/portal.js
@@ -1,48 +1,48 @@
 //共通コンテンツロード
-// function header(){
-//     $.ajax({
-//         url: "header.html",
-//         cache: false,
-//         success: function(html){
-//             document.write(html);
-//         }
-//     });
-// }
-
-
-
-// function footer(){
-//     $.ajax({
-//         url: "footer.html",
-//         cache: false,
-//         success: function(html){
-//             document.write(html);
-//         }
-//     });
-// }
-
-function header(rootDir){
+function header(){
     $.ajax({
-        url: rootDir + "header.html",  // 読み込むHTMLファイル
+        url: "header.html",
         cache: false,
-        async: false,
-        dataType: 'html',
         success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
             document.write(html);
         }
     });
 }
 
-function footer(rootDir){
+
+
+function footer(){
     $.ajax({
-        url: rootDir + "footer.html",  // 読み込むHTMLファイル
+        url: "footer.html",
         cache: false,
-        async: false,
-        dataType: 'html',
         success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
             document.write(html);
         }
     });
 }
+
+// function header(rootDir){
+//     $.ajax({
+//         url: rootDir + "header.html",  // 読み込むHTMLファイル
+//         cache: false,
+//         async: false,
+//         dataType: 'html',
+//         success: function(html){
+//             html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+//             document.write(html);
+//         }
+//     });
+// }
+
+// function footer(rootDir){
+//     $.ajax({
+//         url: rootDir + "footer.html",  // 読み込むHTMLファイル
+//         cache: false,
+//         async: false,
+//         dataType: 'html',
+//         success: function(html){
+//             html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+//             document.write(html);
+//         }
+//     });
+// }
