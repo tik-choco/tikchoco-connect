2d12880cdbdad01c392530c9990850c074e6c368
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-12 16:41:55 +0900

    add Carousel

--- js/page.js
+++ js/page.js
@@ -5,13 +5,13 @@ var breakpoint = 768;//768px以下の場合
 function sliderSet() {
         var windowWidth = window.innerWidth;
         if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
-            slider = $('.slider').bxSlider({
+            slider = $('.news').bxSlider({
             touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-			mode: 'vertical',//縦スライド指定
-			controls: false,//前後のコントロールを表示させない。
-			auto: 'true',//自動的にスライド
-			pager: false//ページ送り無効化
-		});
+      mode: 'vertical',//縦スライド指定
+      controls: false,//前後のコントロールを表示させない。
+      auto: 'true',//自動的にスライド
+      pager: false//ページ送り無効化
+    });
             sliderFlag = true;
         } else if (windowWidth < breakpoint && sliderFlag) {
             slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
@@ -23,45 +23,16 @@ $(window).on('load resize', function() {
         sliderSet();
 });
 
-$('.slider').bxSlider({
-    touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-    mode: 'vertical',
-    controls: false,
-    auto: 'true',
-    pager: false
-    });
+//◆タブレット以下も1行で表示させたい場合は下記のみの記述でOK
+$('.news').bxSlider({
+touchEnabled:false,
+mode: 'vertical',
+controls: false,
+auto: 'true',
+pager: false
+});
 
-	$('.slider2').slick({
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		speed: 500,//スライドのスピード。初期値は300。
-		slidesToShow: 3,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		centerMode: true,//要素を中央ぞろえにする
-		variableWidth: true,//幅の違う画像の高さを揃えて表示
-		dots: true,//下部ドットナビゲーションの表示
-	});
 
-    document.addEventListener('DOMContentLoaded', () => {
 
-        // Get all "navbar-burger" elements
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-      
-        // Add a click event on each of them
-        $navbarBurgers.forEach( el => {
-          el.addEventListener('click', () => {
-      
-            // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
-      
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-      
-          });
-        });
-      
-      });
+
+
