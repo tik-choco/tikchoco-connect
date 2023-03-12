42c50572c8675b9cbd01731e132d283717069db7
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-12 16:07:59 +0900

    up

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    $(".slider").slick({
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      centerMode: true,
+      centerPadding: "30px",
+      dots: true,
+      // fade: true,
+      slidesToShow: 3,
+      speed: 400,
+    });
+  });
