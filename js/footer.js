c853bf7effdcbcfd25e1af3c08e192cb0f315c52
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-03-14 14:38:45 +0900

    up

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,11 @@
 new function(){
 	
 	var footerId = "footer";
-	//メイン
 	function footerFixed(){
-		//ドキュメントの高さ
 		var dh = document.getElementsByTagName("body")[0].clientHeight;
-		//フッターのtopからの位置
 		document.getElementById(footerId).style.top = "0px";
 		var ft = document.getElementById(footerId).offsetTop;
-		//フッターの高さ
 		var fh = document.getElementById(footerId).offsetHeight;
-		//ウィンドウの高さ
 		if (window.innerHeight){
 			var wh = window.innerHeight;
 		}else if(document.documentElement && document.documentElement.clientHeight != 0){
@@ -22,10 +17,7 @@ new function(){
 		}
 	}
 	
-	//文字サイズ
 	function checkFontSize(func){
-	
-		//判定要素の追加	
 		var e = document.createElement("div");
 		var s = document.createTextNode("S");
 		e.appendChild(s);
@@ -34,8 +26,6 @@ new function(){
 		e.style.top="0"
 		document.body.appendChild(e);
 		var defHeight = e.offsetHeight;
-		
-		//判定関数
 		function checkBoxSize(){
 			if(defHeight != e.offsetHeight){
 				func();
@@ -45,7 +35,6 @@ new function(){
 		setInterval(checkBoxSize,1000)
 	}
 	
-	//イベントリスナー
 	function addEvent(elm,listener,fn){
 		try{
 			elm.addEventListener(listener,fn,false);
@@ -61,3 +50,44 @@ new function(){
 	addEvent(window,"resize",footerFixed);
 	
 }
+document.write("<script src='/js/cookie_notification.js'></script> ");
+
+(function() {
+    const expire = 365; 
+    let cc = document.querySelector('.cookie-consent');
+    let ca = document.querySelector('.cookie-agree');
+    const flag = localStorage.getItem('popupFlag');
+    if (flag != null) {
+      const data = JSON.parse(flag);
+      if (data['value'] == 'true') {
+        setTimeout(popup, 2000);
+      } else {
+        const current = new Date();
+        if (current.getTime() > data['expire']) {
+          setWithExpiry('popupFlag', 'true', expire);
+          setTimeout(popup, 2000);
+        }      
+      }
+    } else {
+      setWithExpiry('popupFlag', 'true', expire);
+      setTimeout(popup, 2000);
+    }
+    ca.addEventListener('click', () => {
+      cc.classList.add('cc-hide1');
+      setWithExpiry('popupFlag', 'false', expire);
+    });
+    
+    function setWithExpiry(key, value, expire) {
+      const current = new Date();
+      expire = current.getTime() + expire * 24 * 3600 * 1000;
+      const item = {
+        value: value,
+        expire: expire
+      };
+      localStorage.setItem(key, JSON.stringify(item));
+    }
+    
+    function popup() {
+      cc.classList.add('is-show');
+    }
+  }());
